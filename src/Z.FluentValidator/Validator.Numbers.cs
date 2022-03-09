14c56ce9c5ef86f083b30a3ff3cfa2d54f256658
zhouguoqing <zhouguoqing.eric@gmail.com>
2022-03-09 23:22:48 +0800

    Add  Validator.Numbers

---
+++ src/Z.FluentValidator/Validator.Numbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Z.FluentValidator
+{
+    public partial class Validator
+    {
+        #region " IsNotZero "
+
+        public Validator IsNotZero(int value)
+        {
+            return IsNotZero("", value);
+        }
+
+        public Validator IsNotZero(string name, int value)
+        {
+            return IsNotZero(name, value, string.Format(MessageContainer.IsNotZeroMessage, name));
+        }
+
+        public Validator IsNotZero(string name, int value, string message)
+        {
+            // do the check
+            if (value.IsNotZero())
+            {
+                return NoError();
+            }
+            else
+            {
+                return AddError(name, message);
+            }
+        }
+
+        #endregion
+    }
+}
