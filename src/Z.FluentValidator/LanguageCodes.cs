f347896f7edc47bec15da7826c68ca6e762ddd96
zhouguoqing <zhouguoqing.eric@gmail.com>
2022-03-14 23:25:09 +0800

    add  FluentValidator

---
+++ src/Z.FluentValidator/LanguageCodes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Z.FluentValidator
+{
+    public enum LanguageCodes
+    {
+        /// <summary>
+        /// 英文
+        /// </summary>
+        en_GB,
+
+        /// <summary>
+        /// 中文
+        /// </summary>
+        zh_CN
+    }
+}
