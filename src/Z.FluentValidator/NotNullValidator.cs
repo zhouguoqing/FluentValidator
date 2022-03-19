8cb04b088254e810213da03c5872d2c9391ea690
zhouguoqing <zhouguoqing.eric@gmail.com>
2022-03-19 18:22:05 +0800

    add NotNullValidator

---
+++ src/Z.FluentValidator/NotNullValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Z.FluentValidator
+{
+    /// <summary>
+    /// 非空校验
+    /// </summary>
+    public class NotNullValidator : IDataValidator<string>
+    {
+        public bool Validate(string name, string data, object targetValue, out string tips)
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                tips = name + " 值不能为空！";
+                return false;
+            }
+
+            tips = string.Empty;
+
+            return true;
+        }
+    }
+}
