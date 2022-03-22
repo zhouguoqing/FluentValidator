5f746a7f121545d47dabf6ad585987ed7631a07f
zhouguoqing <zhouguoqing.eric@gmail.com>
2022-03-22 22:55:09 +0800

    add ValueEnumValidator

---
+++ src/Z.FluentValidator/ValueEnumValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Z.FluentValidator
+{
+    class ValueEnumValidator : IDataValidator
+    {
+        public bool Validate(string name, Type type, object data, object targetValue, out string tips)
+        {
+            tips = string.Empty;
+            IValueEnum check = targetValue as IValueEnum;
+            if (check == null)
+                return true;
+            return check.Validate(name, data, null, out tips);
+        }
+    }
+}
